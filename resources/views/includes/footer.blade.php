8d1bc559c6ede5299777761918f129ec96be2ae1
wardahdurrahumniyyah <wardahdurrah12@gmail.com>
2021-03-15 10:25:57 -1200

    first commit blade template

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,11 +2,23 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
 
+<footer class="page-footer bg-dark">
+    <div class="bg-dark">
+      <div class="container">
+        <div class="row py-4 d-flex align-item-center">
+          <div class="col-md-12 text-center">
+            <a href=""><i class="fab fa-instagram text-white mr-4"></i></a>
+            <a href=""><i class="fab fa-facebook text-white mr-4"></i></a>
+            <a href=""><i class="fab fa-pinterest text-white mr-4"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright text-center py-2 bgfooter">
+      <p>&copy; Copyright <a href="#">2021 Wardah Tugas Blade Template Engine</a></p>
+    </div>
+  </footer>
 
-	<br/>
-	<br/>
-	<hr/>
-	<footer>
-		<div align="center" id="copyright text-right">© Copyright 2021 Wardah Blade Template Engine</div>
-	</footer>
+
+
  
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,5 +1,90 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
+<!-- font viga -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+{{-- mystyle --}}
+<style>
+  html {
+  scroll-behavior: smooth;
+}
+
+/* navbar */
+.navbar-brand {
+  font-family: viga;
+  font-size: 20px;
+  color: white;
+}
+#navbar .nav-link {
+  font-weight: 700;
+}
+
+/* footer */
+footer a,
+p,
+h6 {
+  color: black;
+}
+
+.bgfooter {
+  background-color: gray;
+}
+
+/* utility */
+.tombol {
+  text-transform: uppercase;
+  border-radius: 30px;
+  background-color: #3cb7ae;
+  border: none;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+  width: 150px;
+}
+.tombol:hover {
+  background-color: #359b94;
+  margin-top: -4px;
+}
+
+/* desktop */
+@media (min-width: 992px) {
+  .navbar-brand,
+  .nav-link {
+    color: white !important;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.7);
+  }
+  .nav-link {
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+  .nav-link:hover::after {
+    content: "";
+    display: block;
+    border-bottom: solid 3px white;
+    margin: auto;
+    width: 50%;
+    padding-bottom: 5px;
+    margin-bottom: -8px;
+  }
+
+  .jumbotron {
+    margin-top: -75px;
+    height: 640px;
+  }
+
+  .jumbotron .display-4 {
+    font-size: 60px;
+  }
+
+  .content2 {
+    text-align: left;
+  }
+}
+
+</style>
